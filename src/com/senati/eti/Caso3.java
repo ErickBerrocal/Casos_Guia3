1ee5263fb15f8d232dd671125cb39010b2b6b12e
JUAN PC <JUAN PC@JUAN>
2021-03-28 23:42:50 -0500

    Proyecto Terminado Casos_Guia3

---
+++ src/com/senati/eti/Caso3.java
@@ -0,0 +1,25 @@
+package com.senati.eti;
+
+import java.util.Scanner;
+
+public class Caso3 {
+
+	public static void main(String[] args) {
+		Scanner valor = new Scanner(System.in);
+		// Factorial
+		// 1 x 2 x 3 x . . . x n
+
+		int factorial = 1;
+		int n;
+		
+		System.out.println("Ingrese número: ");
+		n = valor.nextInt();
+		for (int i = n; i > 0; i--) {
+			factorial = factorial * i;
+		}
+		System.out.println("El factorial de " + n + " es: " + factorial);;
+		
+		
+	} 
+
+}
---
+++ src/com/senati/eti/Caso7.java
@@ -0,0 +1,23 @@
+package com.senati.eti;
+
+import java.util.Scanner;
+
+public class Caso7 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Ingrese nombre...: ");
+		String nombre = sc.nextLine();
+		
+		// For ascendente de 1 en 1
+		for (int x = 1; x <= 18; x++)
+			System.out.println(x + ". Bienvenido(a) " + nombre + " al ciclo.");
+		
+		// For descendente de 2 en 2
+		for (int j = 10; j >= 1; j -= 2)
+			System.out.println("Valor " + j);
+
+	}
+
+}
